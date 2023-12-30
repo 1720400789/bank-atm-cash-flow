726ee12f886559e8dbf9d22eb323551364bfa048
zjluvcn <hello@zj.cn>
2023-12-30 09:53:14 +0800

    feature: project-service之银行卡相关责任链 Mark 枚举

---
+++ projcet-service/src/main/java/org/zj/atm/project/common/enums/DebitChainMarkEnum.java
@@ -0,0 +1,12 @@
+package org.zj.atm.project.common.enums;
+
+/**
+ * 银行卡相关责任链 Mark 枚举
+ */
+public enum DebitChainMarkEnum {
+
+    /**
+     * 用户注册银行卡过滤器
+     */
+    DEBIT_CARD_REGISTER_FILTER
+}
